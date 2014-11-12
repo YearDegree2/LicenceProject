893306f0a10a60f77662ea20923521f067d907f7
nicobascon <nico.bascon@live.fr>
2014-11-12 15:12:04 +0100

    Begin appli

--- app/app.php
+++ app/app.php
@@ -2,8 +2,20 @@
 
 $app = require_once __DIR__ . '/config/config.php';
 
-$app->get('/', function () use ($app) {
-    return 'Hello';
+use Symfony\Component\HttpFoundation\Request;
+
+$app->get("/", function () use ($app) {
+    return $app['twig']->render('home.html.twig', []);
 });
 
+$app->get("/login", function (Request $request) use ($app) {
+    return $app['twig']->render('login.html.twig', array(
+        'error' => $app['security.last_error']($request),
+        'last_username' => $app['session']->get('_security.last_username'),
+    ));
+})->bind('login');
+
+$app->get("/admin", function () use ($app) {
+    return $app['twig']->render('secret.html.twig', []);
+})->bind('admin');
 return $app;
