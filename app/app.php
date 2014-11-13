0d85f293ff8ceec68ab1af246a8a3bf4124d4b17
nicobascon <nico.bascon@live.fr>
2014-11-13 13:58:59 +0100

    Modify app.php

--- app/app.php
+++ app/app.php
@@ -1,39 +1,39 @@
 <?php
 
-$app = require_once __DIR__ . '/config/config.php';
+$app = require __DIR__ . '/config/config.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-$app->get("/", function () use ($app) {
+$app->get('/', function () use ($app) {
     return new Response(null, 200);
 });
 
-$app->get("/login", function (Request $request) use ($app) {
-    /* A Enlever mais pour l'instant je laisse pour tester */
+$app->get('/login', function (Request $request) use ($app) {
+    /* A Enlever mais pour l'instant je laisse pour tester avec localhost, si on veut tester phpunit, l'enlever*/
     $request->headers->add(array(
-            'Content-language' => 'en',
+            'Content-Type' => 'fr',
         )
     );
     /* Fin a enlever */
-    if ('fr' === $request->headers->get('Content-Language')) {
+    if ('fr' === $request->headers->get('Content-Type')) {
         $file = 'login.fr.html.twig';
     }
-    if ('en' === $request->headers->get('Content-Language')) {
+    if ('en' === $request->headers->get('Content-Type')) {
         $file = 'login.en.html.twig';
     }
     if (isset($file)) {
-        return $app['twig']->render($file, array(
+        return new Response($app['twig']->render($file, array(
             'error' => $app['security.last_error']($request),
             'last_username' => $app['session']->get('_security.last_username'),
-        ));
+        )), 200);
     }
 
     return new Response('Language needed: French or English', 400);
 });
 
-$app->get("/admin", function () use ($app) {
-    return new Response('Admin connecte', 200);
-})->bind('admin');
+$app->get('/admin', function () use ($app) {
+    return new Response(null, 200);
+});
 
 return $app;
