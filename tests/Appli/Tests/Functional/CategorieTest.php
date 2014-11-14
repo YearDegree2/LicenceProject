d0e9b2d0d6d7b8095be593fae829294a7d8f93c2
nicobascon <nico.bascon@live.fr>
2014-11-14 11:50:39 +0100

    Add two tests categorie without connection

--- tests/Appli/Tests/Functional/CategorieTest.php
+++ tests/Appli/Tests/Functional/CategorieTest.php
@@ -14,4 +14,19 @@ class CategorieTest extends WebTestCase
         return $app;
     }
 
+    public function testGetAllCategoriesWithoutConnection()
+    {
+        $client = $this->createClient();
+        $client->request('GET', '/admin/categories');
+        $this->assertEquals(400, $client->getResponse()->getStatusCode());
+        $this->assertEquals('Language needed: French or English', $client->getResponse()->getContent());
+    }
+
+    public function testGetCategorieByIdWithoutConnection()
+    {
+        $client = $this->createClient();
+        $client->request('GET', '/admin/categories/1');
+        $this->assertEquals(400, $client->getResponse()->getStatusCode());
+        $this->assertEquals('Language needed: French or English', $client->getResponse()->getContent());
+    }
 }
