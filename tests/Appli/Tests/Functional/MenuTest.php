c1a96e2b78ba3780a4235356d675576d147578b9
yannisrichard <yannisrichard63@gmail.com>
2014-11-17 12:09:11 +0100

    Add tests rubriquesAll and rubriquesAllWithoutConnection

--- tests/Appli/Tests/Functional/MenuTest.php
+++ tests/Appli/Tests/Functional/MenuTest.php
@@ -43,4 +43,31 @@ class MenuTest extends WebTestCase
         $this->assertEquals('Language needed: French or English', $client->getResponse()->getContent());
     }
 
+    public function testAdminPageRubriquesAll()
+    {
+        $expected = '[{"ID":"1","titre_fr":"home","titre_en":"home","actif":"1","position":"2","date_creation":"2014-11-14","date_modification":"2014-11-14","content_fr":"Bienvenue","content_en":"Welcome","menu_id":"1"},{"ID":"2","titre_fr":"Recherche","titre_en":"Research","actif":"1","position":"3","date_creation":"2014-11-14","date_modification":"2014-11-14","content_fr":"Bienvenue sur mes recherches","content_en":"Welcome","menu_id":"2"},{"ID":"3","titre_fr":"Publications","titre_en":"Publications","actif":"1","position":"7","date_creation":"2014-11-14","date_modification":"2014-11-14","content_fr":"Bienvenue sur mes publications","content_en":"Welcome","menu_id":"3"},{"ID":"4","titre_fr":"Enseignement","titre_en":"Teaching","actif":"1","position":"4","date_creation":"2014-11-14","date_modification":"2014-11-14","content_fr":"Bienvenue sur mes enseignements","content_en":"Welcome","menu_id":"4"},{"ID":"5","titre_fr":"Outils","titre_en":"Tools","actif":"1","position":"5","date_creation":"2014-11-14","date_modification":"2014-11-14","content_fr":"Bienvenue sur mes outils","content_en":"Welcome","menu_id":"5"},{"ID":"6","titre_fr":"Liens","titre_en":"Links","actif":"1","position":"6","date_creation":"2014-11-14","date_modification":"2014-11-14","content_fr":"Bienvenue sur mes liens","content_en":"Welcome","menu_id":"6"}]';
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/admin', array(), array(), array(
+            'CONTENT_TYPE'  => 'fr'
+        ), null);
+        $buttonCrawlerNode = $crawler->selectButton('Envoyer');
+        $form = $buttonCrawlerNode->form(array(
+            '_username' => 'admin',
+            '_password' => 'admin',
+        ));
+        $client->submit($form);
+
+        $client->request('GET', '/admin/rubriques');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals($expected, $client->getResponse()->getContent());
+    }
+
+    public function testAdminPageRubriquesAllWithoutConnection()
+    {
+        $client = $this->createClient();
+        $client->request('GET', '/admin/rubriques');
+        $this->assertEquals(400, $client->getResponse()->getStatusCode());
+        $this->assertEquals('Language needed: French or English', $client->getResponse()->getContent());
+    }
+
 }
