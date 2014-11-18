5a4facf99d1ad48e677596dd4f1cdc16fb0827c9
nicobascon <nico.bascon@live.fr>
2014-11-18 10:34:31 +0100

    Add tests on POST /categorie

--- tests/Appli/Tests/Functional/CategorieTest.php
+++ tests/Appli/Tests/Functional/CategorieTest.php
@@ -14,6 +14,109 @@ class CategorieTest extends WebTestCase
         return $app;
     }
 
+    public function testPostCategorieWithoutConnection()
+    {
+        $client = $this->createClient();
+        $client->request('POST', '/admin/categorie');
+        $this->assertEquals(400, $client->getResponse()->getStatusCode());
+        $this->assertEquals('Language needed: French or English', $client->getResponse()->getContent());
+    }
+
+    public function testPostCategorieWithoutContent()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/admin', array(), array(), array(
+            'CONTENT_TYPE'  => 'en'
+        ), null);
+        $buttonCrawlerNode = $crawler->selectButton('Submit');
+        $form = $buttonCrawlerNode->form(array(
+            '_username' => 'admin',
+            '_password' => 'admin',
+        ));
+        $client->submit($form);
+
+        $client->request('POST', '/admin/categorie', array(), array(), array(), null);
+
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+        $this->assertEquals(null, $client->getResponse()->getContent());
+    }
+
+    public function testPostCategorieWithoutNameFR()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/admin', array(), array(), array(
+            'CONTENT_TYPE'  => 'en'
+        ), null);
+        $buttonCrawlerNode = $crawler->selectButton('Submit');
+        $form = $buttonCrawlerNode->form(array(
+            '_username' => 'admin',
+            '_password' => 'admin',
+        ));
+        $client->submit($form);
+
+        $client->request('POST', '/admin/categorie', array(), array(), array(), '{"name_en":"International and national conferences"}');
+
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+        $this->assertEquals(null, $client->getResponse()->getContent());
+    }
+
+    public function testPostCategorieWithoutNameEN()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/admin', array(), array(), array(
+            'CONTENT_TYPE'  => 'en'
+        ), null);
+        $buttonCrawlerNode = $crawler->selectButton('Submit');
+        $form = $buttonCrawlerNode->form(array(
+            '_username' => 'admin',
+            '_password' => 'admin',
+        ));
+        $client->submit($form);
+
+        $client->request('POST', '/admin/categorie', array(), array(), array(), '{"name_fr":"Conferences nationales et internationales"}');
+
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+        $this->assertEquals(null, $client->getResponse()->getContent());
+    }
+
+    public function testPostCategorieWithoutID()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/admin', array(), array(), array(
+            'CONTENT_TYPE'  => 'en'
+        ), null);
+        $buttonCrawlerNode = $crawler->selectButton('Submit');
+        $form = $buttonCrawlerNode->form(array(
+            '_username' => 'admin',
+            '_password' => 'admin',
+        ));
+        $client->submit($form);
+
+        $client->request('POST', '/admin/categorie', array(), array(), array(), '{"name_fr":"Conferences nationales et internationales","name_en":"International and national conferences"}');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(null, $client->getResponse()->getContent());
+    }
+
+     public function testPostCategorieWithID()
+     {
+         $client = $this->createClient();
+         $crawler = $client->request('GET', '/admin', array(), array(), array(
+             'CONTENT_TYPE'  => 'en'
+         ), null);
+         $buttonCrawlerNode = $crawler->selectButton('Submit');
+         $form = $buttonCrawlerNode->form(array(
+             '_username' => 'admin',
+             '_password' => 'admin',
+         ));
+         $client->submit($form);
+
+         $client->request('POST', '/admin/categorie', array(), array(), array(), '{"ID":2,"name_fr":"Conferences nationales et internationales","name_en":"International and national conferences"}');
+
+         $this->assertEquals(200, $client->getResponse()->getStatusCode());
+         $this->assertEquals(null, $client->getResponse()->getContent());
+    }
+
     public function testGetAllCategoriesWithoutConnection()
     {
         $client = $this->createClient();
@@ -51,7 +154,7 @@ class CategorieTest extends WebTestCase
     public function testGetCategorieByExistingId()
     {
         $client = $this->createClient();
-        $crawler = $client->request('GET', '/admin/categories/1', array(), array(), array(
+        $crawler = $client->request('GET', '/admin/categories/2', array(), array(), array(
             'CONTENT_TYPE'  => 'en'
         ), null);
         $buttonCrawlerNode = $crawler->selectButton('Submit');
@@ -62,7 +165,7 @@ class CategorieTest extends WebTestCase
         $client->submit($form);
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertEquals('{"ID":"1","name_fr":"Conferences nationales et internationales","name_en":"International and national conferences"}',
+        $this->assertEquals('{"ID":"2","name_fr":"Conferences nationales et internationales","name_en":"International and national conferences"}',
                     $client->getResponse()->getContent());
     }
 
