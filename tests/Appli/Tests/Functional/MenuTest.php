733f768e6412c1a17c84ce11bef59a005e17491c
yannisrichard <yannisrichard63@gmail.com>
2014-11-18 17:30:34 +0100

    Add 2 tests PostRubriqueWithId and PostRubriqueWithoutId

--- tests/Appli/Tests/Functional/MenuTest.php
+++ tests/Appli/Tests/Functional/MenuTest.php
@@ -15,6 +15,45 @@ class MenuTest extends WebTestCase
         return $app;
     }
 
+    public function testPostRubriqueWithoutID()
+    {
+    $client = $this->createClient();
+    $crawler = $client->request('GET', '/admin', array(), array(), array(
+        'CONTENT_TYPE'  => 'fr'
+    ), null);
+    $buttonCrawlerNode = $crawler->selectButton('Envoyer');
+    $form = $buttonCrawlerNode->form(array(
+        '_username' => 'admin',
+        '_password' => 'admin',
+    ));
+    $client->submit($form);
+
+    $client->request('POST', '/admin/rubrique', array(), array(), array(), '{"titre_fr":"test1fr","titre_en":"test1en","actif":1,"position":7}');
+
+    $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    $this->assertEquals(null, $client->getResponse()->getContent());
+
+    }
+
+    public function testPostRubriqueWithID()
+    {
+    $client = $this->createClient();
+    $crawler = $client->request('GET', '/admin', array(), array(), array(
+        'CONTENT_TYPE'  => 'fr'
+    ), null);
+    $buttonCrawlerNode = $crawler->selectButton('Envoyer');
+    $form = $buttonCrawlerNode->form(array(
+        '_username' => 'admin',
+        '_password' => 'admin',
+    ));
+    $client->submit($form);
+
+    $client->request('POST', '/admin/rubrique', array(), array(), array(), '{"ID":7,"titre_fr":"test2fr","titre_en":"test2en","actif":1,"position":7}');
+
+    $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    $this->assertEquals(null, $client->getResponse()->getContent());
+    }
+
     public function testGetMenuAll()
     {
         $expected = '[{"ID":"1","titre_fr":"home","titre_en":"home","actif":"1","position":"2"},{"ID":"2","titre_fr":"Recherche","titre_en":"Research","actif":"1","position":"3"},{"ID":"3","titre_fr":"Publications","titre_en":"Publications","actif":"1","position":"7"},{"ID":"4","titre_fr":"Enseignement","titre_en":"Teaching","actif":"1","position":"4"},{"ID":"5","titre_fr":"Outils","titre_en":"Tools","actif":"1","position":"5"},{"ID":"6","titre_fr":"Liens","titre_en":"Links","actif":"1","position":"6"}]';
@@ -106,7 +145,7 @@ class MenuTest extends WebTestCase
         $this->assertEquals(400, $client->getResponse()->getStatusCode());
     }
 
-    public function testGetRubriquesByIdWithoutConnection()
+    public function testGetRubriqueByIdWithoutConnection()
     {
         $client = $this->createClient();
         $client->request('GET', '/admin/rubriques/3');
