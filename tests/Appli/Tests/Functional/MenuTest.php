7b31bbdeb4da907a55609731e04a9ad653fd8d7e
yannisrichard <yannisrichard63@gmail.com>
2014-11-18 11:02:29 +0100

    Add test GetRubriqueByNonExistingId and correct tests names

--- tests/Appli/Tests/Functional/MenuTest.php
+++ tests/Appli/Tests/Functional/MenuTest.php
@@ -15,7 +15,7 @@ class MenuTest extends WebTestCase
         return $app;
     }
 
-    public function testAdminPageMenuAll()
+    public function testGetMenuAll()
     {
         $expected = '[{"ID":"1","titre_fr":"home","titre_en":"home","actif":"1","position":"2"},{"ID":"2","titre_fr":"Recherche","titre_en":"Research","actif":"1","position":"3"},{"ID":"3","titre_fr":"Publications","titre_en":"Publications","actif":"1","position":"7"},{"ID":"4","titre_fr":"Enseignement","titre_en":"Teaching","actif":"1","position":"4"},{"ID":"5","titre_fr":"Outils","titre_en":"Tools","actif":"1","position":"5"},{"ID":"6","titre_fr":"Liens","titre_en":"Links","actif":"1","position":"6"}]';
         $client = $this->createClient();
@@ -35,7 +35,7 @@ class MenuTest extends WebTestCase
 
     }
 
-    public function testAdminPageMenuAllWithoutConnection()
+    public function testGetMenuAllWithoutConnection()
     {
         $client = $this->createClient();
         $client->request('GET', '/admin/menus');
@@ -43,7 +43,7 @@ class MenuTest extends WebTestCase
         $this->assertEquals('Language needed: French or English', $client->getResponse()->getContent());
     }
 
-    public function testAdminPageRubriquesAll()
+    public function testGetRubriquesAll()
     {
         $expected = '[{"ID":"1","titre_fr":"home","titre_en":"home","actif":"1","position":"2","date_creation":"2014-11-14","date_modification":"2014-11-14","content_fr":"Bienvenue","content_en":"Welcome","menu_id":"1"},{"ID":"2","titre_fr":"Recherche","titre_en":"Research","actif":"1","position":"3","date_creation":"2014-11-14","date_modification":"2014-11-14","content_fr":"Bienvenue sur mes recherches","content_en":"Welcome","menu_id":"2"},{"ID":"3","titre_fr":"Publications","titre_en":"Publications","actif":"1","position":"7","date_creation":"2014-11-14","date_modification":"2014-11-14","content_fr":"Bienvenue sur mes publications","content_en":"Welcome","menu_id":"3"},{"ID":"4","titre_fr":"Enseignement","titre_en":"Teaching","actif":"1","position":"4","date_creation":"2014-11-14","date_modification":"2014-11-14","content_fr":"Bienvenue sur mes enseignements","content_en":"Welcome","menu_id":"4"},{"ID":"5","titre_fr":"Outils","titre_en":"Tools","actif":"1","position":"5","date_creation":"2014-11-14","date_modification":"2014-11-14","content_fr":"Bienvenue sur mes outils","content_en":"Welcome","menu_id":"5"},{"ID":"6","titre_fr":"Liens","titre_en":"Links","actif":"1","position":"6","date_creation":"2014-11-14","date_modification":"2014-11-14","content_fr":"Bienvenue sur mes liens","content_en":"Welcome","menu_id":"6"}]';
         $client = $this->createClient();
@@ -62,7 +62,7 @@ class MenuTest extends WebTestCase
         $this->assertEquals($expected, $client->getResponse()->getContent());
     }
 
-    public function testAdminPageRubriquesAllWithoutConnection()
+    public function testGetRubriquesAllWithoutConnection()
     {
         $client = $this->createClient();
         $client->request('GET', '/admin/rubriques');
@@ -70,7 +70,7 @@ class MenuTest extends WebTestCase
         $this->assertEquals('Language needed: French or English', $client->getResponse()->getContent());
     }
 
-    public function testAdminPageRubriquesId()
+    public function testGetRubriquesById()
     {
         $expected = '{"ID":"3","titre_fr":"Publications","titre_en":"Publications","actif":"1","position":"7","date_creation":"2014-11-14","date_modification":"2014-11-14","content_fr":"Bienvenue sur mes publications","content_en":"Welcome","menu_id":"3"}';
         $client = $this->createClient();
@@ -89,7 +89,24 @@ class MenuTest extends WebTestCase
         $this->assertEquals($expected, $client->getResponse()->getContent());
     }
 
-    public function testAdminPageRubriquesIdWithoutConnection()
+    public function testGetRubriqueByNonExistingId()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/admin', array(), array(), array(
+            'CONTENT_TYPE'  => 'fr'
+        ), null);
+        $buttonCrawlerNode = $crawler->selectButton('Envoyer');
+        $form = $buttonCrawlerNode->form(array(
+            '_username' => 'admin',
+            '_password' => 'admin',
+        ));
+        $client->submit($form);
+
+        $client->request('GET', '/admin/rubriques/1000');
+        $this->assertEquals(400, $client->getResponse()->getStatusCode());
+    }
+
+    public function testGetRubriquesByIdWithoutConnection()
     {
         $client = $this->createClient();
         $client->request('GET', '/admin/rubriques/3');
